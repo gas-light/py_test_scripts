cbe7a1dd915d1e2b997bdd4c1476190800be272a
gas-light <nidayede1239@gmail.com>
2021-05-04 13:05:42 +0800

    0504_test

---
+++ read.py
@@ -0,0 +1,13 @@
+str_line=""
+sources_list = []
+with open(".\sources.list",'r') as file:
+    for line in file:
+        var = line.split("\n")
+        str_line = "echo " + "\"" + var[0] + "\"" + " >> 1.list"
+        sources_list.append(str_line)
+
+with open("echo.txt","w+") as file:
+    for i in sources_list:
+        file.write(i)
+        file.write("\n")
+

e56dac59d3a0da05a100708f1f545b89c0ec16dc
gas-light <213163651@seu.edu.cn>
2021-05-04 13:38:46 +0800

    Add some Wear leveling scripts

---
+++ WL/tools/New Text Document.bat
@@ -0,0 +1,6 @@
+@echo off
+set aa=Great China,Proud of you.
+echo Before:%aa%
+echo After:%aa:China=Public China%
+echo aa=%aa%
+pause
---
+++ WL/tools/Test.bat
@@ -0,0 +1,4 @@
+@ECHO OFF
+Call C:\Users\User\Documents\WL_Rainier\tools\JESD219_client.exe \\.\Physicaldrive1, UI:2
+echo ERR=%ERRORLEVEL%
+
